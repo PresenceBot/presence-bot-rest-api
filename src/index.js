89f79b2f12fb85d691c5561d38a899c38c4b6d60
Alex Babis <acbabis@gmail.com>
2018-02-23 15:54:33 -0700

    Setup

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const session = require('express-session');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const notFound = require('./routes/not-found.js');
+
+const { SESSION_SECRET } = process.env;
+
+if(!SESSION_SECRET) {
+    throw new Error('Environment variable SESSION_SECRET should be set');
+}
+
+const app = express();
+app.use(compression());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(session({ secret: SESSION_SECRET }));
+
+app.use('/guilds', require('./routes/guilds/guilds.js'));
+
+app.use(notFound);
+
+app.listen(process.env.PORT || 8080);
